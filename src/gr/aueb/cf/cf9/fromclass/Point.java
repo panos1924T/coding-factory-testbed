db5c21479334aecaf10ea219b468d4dcb0a63dc8
pants <pants.ath@gmail.com>
2026-01-15 19:24:38 +0200

    feat: add immutable class point

---
+++ src/gr/aueb/cf/cf9/fromclass/Point.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.cf9.fromclass;
+
+public class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}

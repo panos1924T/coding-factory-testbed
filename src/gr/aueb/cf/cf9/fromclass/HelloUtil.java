184b9371a82115b0fca3b01842294ba009fbb0e7
pants <pants.ath@gmail.com>
2026-01-16 16:38:21 +0200

    feat: add utility class hello util

---
+++ src/gr/aueb/cf/cf9/fromclass/HelloUtil.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.cf9.fromclass;
+
+public final class HelloUtil {
+
+    private HelloUtil(){}
+
+    public static void sayHello() {
+        System.out.println("Hello Programmers");
+    }
+}

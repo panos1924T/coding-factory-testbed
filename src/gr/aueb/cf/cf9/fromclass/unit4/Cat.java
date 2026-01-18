efe3dbd958b0065618ad0ea8dec7c2e5e52ff468
pants <pants.ath@gmail.com>
2026-01-18 18:22:00 +0200

    feat: add classes which implements the interface

---
+++ src/gr/aueb/cf/cf9/fromclass/unit4/Cat.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.fromclass.unit4;
+
+public class Cat implements ISpeakable{
+
+    @Override
+    public void speak() {
+        System.out.println("Meow");
+    }
+}
---
+++ src/gr/aueb/cf/cf9/fromclass/unit4/Dog.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.fromclass.unit4;
+
+public class Dog implements ISpeakable{
+
+    @Override
+    public void speak() {
+        System.out.println("Woof");
+    }
+}

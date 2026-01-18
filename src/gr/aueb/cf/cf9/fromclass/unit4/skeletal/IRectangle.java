3289ceace12fddb237e7392ba6b758005829644e
pants <pants.ath@gmail.com>
2026-01-18 20:50:02 +0200

    feat: add interface IRectangle

---
+++ src/gr/aueb/cf/cf9/fromclass/unit4/skeletal/IRectangle.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.cf9.fromclass.unit4.skeletal;
+
+public interface IRectangle {
+
+    public double getArea();
+
+    public double getPerimeter();
+
+    public boolean isSquare();
+}

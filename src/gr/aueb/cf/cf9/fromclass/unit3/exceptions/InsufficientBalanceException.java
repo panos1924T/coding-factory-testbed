2c374ae4b6eafaad507146f9b34d00c494bf0d4a
pants <pants.ath@gmail.com>
2026-01-18 18:06:24 +0200

    feat: add custom exceptions

---
+++ src/gr/aueb/cf/cf9/fromclass/unit3/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.cf9.fromclass.unit3.exceptions;
+
+public class InsufficientBalanceException extends Exception{
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
---
+++ src/gr/aueb/cf/cf9/fromclass/unit3/exceptions/NegativeAmountException.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.cf9.fromclass.unit3.exceptions;
+
+public class NegativeAmountException extends Exception{
+
+    public NegativeAmountException(String message) {
+        super(message);
+    }
+}
